b69987804fed6590466a4eb76a9dc2cf9e52f718
saurabh jain <luckyjain0107@gmai.com>
2022-10-05 19:32:04 +0530

    Read Number and Display Word and Weekday

---
+++ IfAndElseStatement/ReadNumberAndDisplayWeekDay.js
@@ -0,0 +1,12 @@
+// const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
+// var number = prompt("Enter Number between 1 to 7 ");
+// var numbers=[null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+// console.log(numbers[number]);
+console.log("hello world");
+const prompt = require('prompt-sync')();
+
+var fname = prompt("enter your first name");
+var lname = prompt("enter your last name");
+console.log("Your full name is",fname*lname);
--- IfAndElseStatement/ReadNumberAndWriteInWord.js
+++ IfAndElseStatement/ReadNumberAndWriteInWord.js
@@ -1,5 +1,3 @@
-const prompt = require('prompt-sync')();
 let number = prompt("enter number between 0 to 9");
 let numbers=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
 console.log(numbers[number]);
