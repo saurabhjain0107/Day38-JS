3a0d43bf458ef4f360323391d12aba4786e4ba08
saurabh jain <luckyjain0107@gmai.com>
2022-10-05 19:06:39 +0530

    Read Number And Write In Word

---
+++ IfAndElseStatement/ReadNumberAndWriteInWord.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+let number = prompt("enter number between 0 to 9");
+let numbers=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+console.log(numbers[number]);
